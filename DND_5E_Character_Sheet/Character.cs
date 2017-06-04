d9d4050ac9888b01dc1caed1771e54991e6bd419
ElahiKeru <carnatorian@gmail.com>
2017-06-04 17:19:59 -0500

    Beginning population of Character and Statistics Classes

---
+++ DND_5E_Character_Sheet/Character.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DND_5E_Character_Sheet
+{
+    public class Character
+    {
+        #region Fields
+        private List<string> characterAbilities;
+        private List<string> characterInventory;
+        private int characterLevel;
+        private int characterProficiency;
+        private int characterSaves;
+        private int characterSkills;
+        private Statistics characterStatistics;
+        private int characterVocationList;
+        private string characterName;
+        private string characterPlayer;
+        private string characterEthics;
+        private string characterMorals;
+        #endregion
+
+        #region Properties
+        public int CharacterSkills
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int CharacterSaves
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public List<string> CharacterInventory
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public List<string> CharacterAbilities
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Returns integer: Character's Proficiency Bonus
+        /// </summary>
+        public int CharacterProficiency
+        {
+            get
+            {
+                return characterProficiency;
+            }
+
+            set
+            {
+                characterProficiency = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns integer: Character's Total Level
+        /// </summary>
+        public int CharacterLevel
+        {
+            get
+            {
+                return characterLevel;
+            }
+
+            set
+            {
+                characterLevel = value;
+            }
+        }
+
+        public int CharacterVocationList
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Returns string: Character's Name
+        /// </summary>
+        public string CharacterName
+        {
+            get
+            {
+                return characterName;
+            }
+
+            set
+            {
+                characterName = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns string: Character's Player's Name
+        /// </summary>
+        public string CharacterPlayer
+        {
+            get
+            {
+                return characterPlayer;
+            }
+
+            set
+            {
+                characterPlayer = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns string: Character's inclination towards Law or Chaos
+        /// </summary>
+        public string CharacterEthics
+        {
+            get
+            {
+                return characterEthics;
+            }
+
+            set
+            {
+                characterEthics = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns string: Character's inclination towards Good or Evil
+        /// </summary>
+        public string CharacterMorals
+        {
+            get
+            {
+                return characterMorals;
+            }
+
+            set
+            {
+                characterMorals = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns lazy-loaded reference to Statistics object
+        /// </summary>
+        public Statistics CharacterStatistics
+        {
+            get
+            {
+                if(characterStatistics == null)
+                {
+                    characterStatistics = new Statistics();
+                }
+
+                return characterStatistics;
+            }
+
+            set
+            {
+                characterStatistics = value;
+            }
+        }
+        #endregion
+    }
+}
---
+++ DND_5E_Character_Sheet/Statistics.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq;
+
+namespace DND_5E_Character_Sheet
+{
+    public class Statistics
+    {
+        #region Fields
+        private int strengthScore;
+        private int strengthMod;
+        private int dexterityScore;
+        private int dexterityMod;
+        private int constitutionScore;
+        private int constitutionMod;
+        private int intelligenceScore;
+        private int intelligenceMod;
+        private int wisdomScore;
+        private int wisdomMod;
+        private int charismaScore;
+        private int charismaMod;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Returns integer: Strength Score
+        /// </summary>
+        public int StrengthScore
+        {
+            get
+            {
+                return strengthScore;
+            }
+
+            set
+            {
+                strengthScore = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns integer: Strength Modifier
+        /// </summary>
+        public int StrengthMod
+        {
+            get
+            {
+                return strengthMod;
+            }
+
+            set
+            {
+                strengthMod = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns integer: Dexterity Score
+        /// </summary>
+        public int DexterityScore
+        {
+            get
+            {
+                return dexterityScore;
+            }
+
+            set
+            {
+                dexterityScore = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns integer: Dexterity Modifier
+        /// </summary>
+        public int DexterityMod
+        {
+            get
+            {
+                return dexterityMod;
+            }
+
+            set
+            {
+                dexterityMod = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns integer: Constitution Score
+        /// </summary>
+        public int ConstitutionScore
+        {
+            get
+            {
+                return constitutionScore;
+            }
+
+            set
+            {
+                constitutionScore = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns integer: Constitution Modifier
+        /// </summary>
+        public int ConstitutionMod
+        {
+            get
+            {
+                return constitutionMod;
+            }
+
+            set
+            {
+                constitutionMod = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns integer: Intelligence Score
+        /// </summary>
+        public int IntelligenceScore
+        {
+            get
+            {
+                return intelligenceScore;
+            }
+
+            set
+            {
+                intelligenceScore = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns integer: Intelligence Modifier
+        /// </summary>
+        public int IntelligenceMod
+        {
+            get
+            {
+                return intelligenceMod;
+            }
+
+            set
+            {
+                intelligenceMod = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns integer: Wisdom Score
+        /// </summary>
+        public int WisdomScore
+        {
+            get
+            {
+                return wisdomScore;
+            }
+
+            set
+            {
+                wisdomScore = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns integer: Wisdom Modifier
+        /// </summary>
+        public int WisdomMod
+        {
+            get
+            {
+                return wisdomMod;
+            }
+
+            set
+            {
+                wisdomMod = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns integer: Charisma Score
+        /// </summary>
+        public int CharismaScore
+        {
+            get
+            {
+                return charismaScore;
+            }
+
+            set
+            {
+                charismaScore = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns integer: Charisma Modifier
+        /// </summary>
+        public int CharismaMod
+        {
+            get
+            {
+                return charismaMod;
+            }
+
+            set
+            {
+                charismaMod = value;
+            }
+        }
+        #endregion
+    }
+}
